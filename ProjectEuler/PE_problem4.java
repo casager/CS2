f490f7cb6642b99d5489d3e4656f642f01de3491
unknown <carson.sager@okstate.edu>
2024-03-27 13:10:26 -0500

    PE

---
+++ ProjectEuler/PE_problem4.java
@@ -0,0 +1,9 @@
+public class PE_problem4 {
+    public static void main(String[] args) {
+        for (int i = 100; i < 1000; i++){
+            for(int j = i + 1; j< 1000; j++){ //setting at i + 1 gets rid duplicates
+
+            }
+        }
+    }
+}
