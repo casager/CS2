074c81d891b17c1dce0712c61932094091cdbfa5
casager <carson.sager@okstate.edu>
2024-04-11 17:36:07 -0500

    upd

---
+++ Assignment5/SentimentAnalysis.java
@@ -0,0 +1,11 @@
+public class SentimentAnalysis {
+    class ReviewEntry{
+        private int sentenceID;
+        private int totalScore;
+        private int num_phrases;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
